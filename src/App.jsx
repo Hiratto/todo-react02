536518c899a3fddcfa592c6f01499f1fba369586
Takahisa Hirano <54578410+Hiratto@users.noreply.github.com>
2022-02-25 17:18:19 +0900

    HTMLの基礎作り

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,45 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      {/* インプットエリア */}
+      <div>
+        <input placeholder="TODOを入力" type="text" name="" id="" />
+        <button>追加</button>
+      </div>
+
+      {/* 未完了リスト */}
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>ああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>いいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+
+      {/* 完了リスト */}
+      <div>
+        <p>完了したTODO</p>
+        <ul>
+          <div>
+            <li>うううう</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>ええええ</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
